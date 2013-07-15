e8305b729528520829dcc49fff53323b718fb6c5
dlymanicole <nicoledlyma@gmail.com>
2013-07-15 01:50:35 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,8 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var buf = new Buffer(100);
-buf = fs.readFilesync(index.html);
-var str = buf.toString(buf);
+var len = buf.write(fs.readFilesync(index.html));
+var str = buf.toString('utf-8', 0, len);
 
 
 app.get('/', function(request, response) {
