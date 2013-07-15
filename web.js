c71ee21278034bcc0bb804e9c300e4850e76bba2
dlymanicole <nicoledlyma@gmail.com>
2013-07-15 03:01:21 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var str = fs.readFileSync('./bitstarter/index.html').toString();
+var str = fs.readFileSync(__dirname + '/index.html').toString();
 
 app.get('/', function(request, response) {
-  response.send("%s", str);
+  response.send("The string: %s", str);
 });
 
 var port = process.env.PORT || 5000;
