2052d06a878fbe46ff1c383feab994110dac2d91
dlymanicole <nicoledlyma@gmail.com>
2013-07-15 01:35:35 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+buf = new Buffer(fs.readFilesync(index.html));
+str = buf.toString(buf);
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
